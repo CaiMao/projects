cc7cf7229418376ac00674a312c1ac1690e2b3e9
CaiMao <328691091@qq.com>
2014-11-12 17:32:54 +0800

    另外一个地方第一次修该

--- project-core/src/main/java/com/cm/project/core/TestNewClass.java
+++ project-core/src/main/java/com/cm/project/core/TestNewClass.java
@@ -1,10 +1,10 @@
 package com.cm.project.core;
-
 public class TestNewClass {
 
 	public static void main(String[] args) {
 		System.out.println("我是core工程的第一个类");
 		System.out.println("第一次修改这个文件");
+		System.out.println("另外一个地方第一次修改文件");
 	}
 
 }
