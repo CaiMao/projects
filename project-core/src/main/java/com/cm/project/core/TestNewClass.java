3ca1b2efec4e475488bbcdd01b00dba8af23398c
CaiMao <328691091@qq.com>
2014-11-12 17:36:24 +0800

    第二次修改

--- project-core/src/main/java/com/cm/project/core/TestNewClass.java
+++ project-core/src/main/java/com/cm/project/core/TestNewClass.java
@@ -5,6 +5,8 @@ public class TestNewClass {
 		System.out.println("我是core工程的第一个类");
 		System.out.println("第一次修改这个文件");
 		System.out.println("另外一个地方第一次修改文件");
+		System.out.println("另外一个地方第二次修改文件");
+		
 	}
 
 }
